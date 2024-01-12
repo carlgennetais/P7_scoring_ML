efa9540aa18bb6fe3a42cc050713bd81808c2c68
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-01-12 17:01:52 +0100

    update api

--- src/api/api.py
+++ src/api/api.py
@@ -1,22 +1,28 @@
+"""
+Main API code
+"""
+
+
+# builtin
 import pickle
 from typing import Union
 
+# libraires
 import pandas as pd
 from fastapi import FastAPI, HTTPException
+
+
+# source code
+from .loaders import DataLoader
+
 
 app = FastAPI()
 
 # load cleaned-transformed dataframe once
 # use raw data instead for interpretability ?
-customers = (
-    pd.read_pickle("../../data/processed/app_train_cleaned.pkl")
-    .set_index("SK_ID_CURR")
-    .drop("TARGET", axis=1)
-)
 
-# load model
-with open("../../models/model.pkl", "rb") as f:
-    model = pickle.load(f)
+
+data = DataLoader.df()
 
 
 @app.get("/")
@@ -31,30 +37,42 @@ def ping():
 
 @app.get("/customers")
 def list_customers():
+    """ADD A DOCSTRING HERE"""
+
     # TODO performance
-    return customers.head(1000).index.to_list()
+    return data.head(100).index.to_list()
 
 
 @app.get("/customers/{customer_id}")
 def read_single_customer(customer_id: int):
-    if customer_id in customers.index:
-        return customers.loc[customer_id, :].fillna("").to_dict()
-    else:
+    """ADD A DOCSTRING HERE"""
+
+    if customer_id not in data.index:
         raise HTTPException(status_code=404, detail="Customer ID does not exist")
+
+    return data.loc[customer_id, :].fillna("").to_dict()
 
 
 @app.get("/customers_stats")
 def all_customers_stats():
-    return customers.describe().loc[["count", "mean", "std"], :].to_dict()
+    """ADD A DOCSTRING HERE"""
+
+    return data.describe().loc[["count", "mean", "std"], :].to_dict()
 
 
 # @app.get("/customers/{customer_id}/predict")
 @app.get("/predict/{customer_id}")
 def predict(customer_id: int, q: Union[str, None] = None):
-    if customer_id in customers.index:
-        return model.predict_proba(pd.DataFrame(customers.loc[customer_id, :]).T)
-    else:
+    """ADD A DOCSTRING HERE"""
+
+    if customer_id not in data.index:
         raise HTTPException(status_code=404, detail="Customer ID does not exist")
+
+    model = DataLoader.model()
+
+    pred = model.predict_proba(pd.DataFrame(data.loc[customer_id, :]).T)
+
+    return {"prediction": pred}
 
 
 @app.get("/shap/{customer_id}")
